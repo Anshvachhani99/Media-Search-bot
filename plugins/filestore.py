6ab7c90572f7b71ff478692aa244175b5e516178
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2022-08-06 15:09:21 +0530

    Update filestore.py

--- plugins/filestore.py
+++ plugins/filestore.py
@@ -26,7 +26,7 @@
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from utils import temp, unpack_new_file_id
+from utils import unpack_new_file_id
 
 # https://github.com/EvamariaTG/EvaMaria/blob/e325a1d8ef484ecc67bad74e1a02173ee0505801/plugins/genlink.py#L23
 
@@ -41,5 +41,5 @@ async def gen_link_s(bot, message):
         return await message.reply("__**Reply to a Supported Media**__")
 
     file_id, ref = unpack_new_file_id((getattr(replied, file_type)).file_id)
-    url = f"https://t.me/{temp.Bot_Username}?start=muhammedrk-mo-tech-group-{file_id}"
+    url = f"https://t.me/Testv103robot?start=muhammedrk-mo-tech-group-{file_id}"
     await message.reply(f"**Here is your Link:**\n`{url}")
